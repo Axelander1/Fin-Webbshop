36fe7b1745acabc592b258957de948b9e8ba719e
Axelander1 <alexander.botker@elev.ga.ntig.se>
2023-02-13 16:05:28 +0100

    13/02
    Har fixat javascripten för min sidebar, och behöver nu bara arbeta med HTML och CSS koden för detta.

---
+++ script.js
@@ -0,0 +1,32 @@
+// Script har bearbetats, men hämtades ursprungligen från:
+    https://www.w3schools.com/howto/howto_js_collapse_sidebar.asp //
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("mySidebar").style.marginLeft = "2%";
+  }
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("mySidebar").style.marginLeft = "0%";} 
+
+// Delvis hämtad från 
+   https://stackoverflow.com/questions/52049021/if-statement-to-check-css-property-not-working
+
+function deciderFunction() {
+    var sidebar = document.getElementById("mySidebar");
+
+    // Get the computed style, that is the combination of styles 
+    // resulting from your CSS classlist, etc
+    var computedStyle = window.getComputedStyle(sidebar, null); 
+
+    // Get visibility value from computed styles
+    var widthValue = computedStyle.getPropertyValue("width")
+
+    if (widthValue == "250px" ) {
+        closeNav()
+    } 
+    else {
+        openNav()
+    }
+
+}
